598a706ee119f5343a06414272abb30c3b931e5c
YeonSM <tmdals4544@gnu.ac.kr>
2025-12-31 12:33:47 +0900

    초기 커밋 및 ignore 수정

---
+++ src/main/java/com/ch/gittest/config/spring/WebConfig.java
@@ -0,0 +1,13 @@
+package com.ch.gittest.config.spring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.ch.gittest.controller"})
+public class WebConfig {
+
+	
+}
---
+++ src/main/java/com/ch/gittest/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.ch.gittest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/test")
+	@ResponseBody
+	public String main() {
+		return "YSM";
+	}
+	
+}
